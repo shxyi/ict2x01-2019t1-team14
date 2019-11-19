aa1d3c7d6b0a230e6bab0a621cc679cddf1f10e6
rramyaa <54789348+rramyaa@users.noreply.github.com>
2019-11-19 23:15:45 +0800

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ const routes: Routes = [
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
   { path: 'direction', loadChildren: './direction/direction.module#DirectionPageModule' },
-  { path: 'feedback', loadChildren: './feedback/feedback.module#FeedbackPageModule' },  { path: 'conquered', loadChildren: './conquered/conquered.module#ConqueredPageModule' },
+  { path: 'feedback', loadChildren: './feedback/feedback.module#FeedbackPageModule' },
   { path: 'conquer', loadChildren: './conquer/conquer.module#ConquerPageModule' },
   { path: 'btn-conquer', loadChildren: './btn-conquer/btn-conquer.module#BtnConquerPageModule' },
 
