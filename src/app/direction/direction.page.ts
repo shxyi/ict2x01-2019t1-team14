0ae2c98ef9c49b457ddaf6c12737cb75ce45fb97
MDYudi <1801559@sit.singaporetech.edu.sg>
2019-11-24 15:13:12 +0800

    distance fix

--- src/app/direction/direction.page.ts
+++ src/app/direction/direction.page.ts
@@ -46,6 +46,7 @@ export class DirectionPage implements OnInit, AfterViewInit {
   drawMang;
   savedCircle;
   distance = 0;
+  finaldist = 0;
 
   buttonDisabled = false;
 
@@ -529,7 +530,7 @@ export class DirectionPage implements OnInit, AfterViewInit {
     var dLat = this.toRad((lat2 - lat1));
     var dLon = this.toRad((lon2 - lon1));
     var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+      Math.cos(this.toRad(lat1)) * Math.cos(this.toRad(lat2)) *
       Math.sin(dLon / 2) * Math.sin(dLon / 2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     var d = R * c;
